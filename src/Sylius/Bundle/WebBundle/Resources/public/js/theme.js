99ae01b5438f98b1cf9539df6652a05d0e35d63f
Diego <songecko@gmail.com>
2014-04-29 04:58:05 -0300

    fix js

--- src/Sylius/Bundle/WebBundle/Resources/public/js/theme.js
+++ src/Sylius/Bundle/WebBundle/Resources/public/js/theme.js
@@ -4,25 +4,7 @@
 // 	services is used for Services (services.html)
 // 
 
-$(function () {
-	slideshow.initialize();
 
-	services.initialize();
-
-	contactForm.initialize();
-
-
-	// retina display
-	if(window.devicePixelRatio >= 1.2){
-	    $("[data-2x]").each(function(){
-	        if(this.tagName == "IMG"){
-	            $(this).attr("src",$(this).attr("data-2x"));
-	        } else {
-	            $(this).css({"background-image":"url("+$(this).attr("data-2x")+")"});
-	        }
-	    });
-	}
-});
 
 window.utils = {
 	isFirefox: function () {
@@ -144,3 +126,23 @@ var slideshow = {
 		}
 	}
 }
+
+$(function () {
+	slideshow.initialize();
+
+	services.initialize();
+
+	contactForm.initialize();
+
+
+	// retina display
+	if(window.devicePixelRatio >= 1.2){
+	    $("[data-2x]").each(function(){
+	        if(this.tagName == "IMG"){
+	            $(this).attr("src",$(this).attr("data-2x"));
+	        } else {
+	            $(this).css({"background-image":"url("+$(this).attr("data-2x")+")"});
+	        }
+	    });
+	}
+});
